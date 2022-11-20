2d2828db88da1bc354ecd9e0c3a4080f0eb1626f
hyuntae <bht9011@gmail.com>
2022-11-21 07:20:00 +0900

    jwt tutorial

---
+++ src/main/java/com/coen/jwt/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.coen.jwt.controller;
+
+import com.coen.jwt.dto.UserDto;
+import com.coen.jwt.entity.Member;
+import com.coen.jwt.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Member> signup(@Valid @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<Member> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<Member> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+
+}
